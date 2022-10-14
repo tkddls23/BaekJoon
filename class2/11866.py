a3940e76bdc03ce4b3d58c58c3976b77c30cc1c4
wonseok <tkddls23@konkuk.ac.kr>
2022-10-15 03:20:03 +0900

    baekjoon_11866

--- class2/11866.py
+++ class2/11866.py
@@ -1,20 +1,17 @@
 # 11866 요세푸스 문제 0
+import sys
 
-NK = list(map(int,input().split()))
+NK = list(map(int,sys.stdin.readline().split()))
 N = NK[0]
 K = NK[1]
+arr = [i for i in range(1,N+1)]
 result = []
+tmp = 0
 
-def shift(nList,x):
-    tmp = nList.pop(x)
-    print(tmp)
-    result.append(tmp)
+for i in range(N):
+    tmp += K-1  
+    if tmp >= len(arr):   # 한바퀴를 돌고 그다음으로 돌아올때를 대비해 값을 나머지로 바꿈  
+        tmp = tmp%len(arr)
+    result.append(str(arr.pop(tmp)))
 
-nList = [i+1 for i in range(N)]
-
-while nList:
-    K = K % N
-    shift(nList,K-1)
-    K += 3
-
-print(result)
+print("<",", ".join(result)[:],">", sep='')
