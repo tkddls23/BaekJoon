e2290709c68cc5fc7d55f12b21e2972829bb7fa9
wonseok <tkddls23@konkuk.ac.kr>
2022-10-16 00:25:41 +0900

    baekjoon_10816

--- class2/10816_hash.py
+++ class2/10816_hash.py
@@ -5,18 +5,19 @@ cardNum = list(map(int,sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 hasCardNum = list(map(int,sys.stdin.readline().split()))
 
-cardSet = set(cardNum)
-hasCardSet = set(hasCardNum)
-difference = hasCardSet - cardSet
+hash = {}
 result = []
-resultList = result.append
 
+for i in cardNum:
+    if i in hash:
+        hash[i] += 1
+    else:
+        hash[i] = 1
 
 for i in hasCardNum:
-    if i in difference:
-        resultList(0)
+    if i in hash:
+        result.append(hash[i])
     else:
-        resultList(cardNum.count(i))
+        result.append(0)
 
-print(result)
-
+print(" ".join(map(str, result)))
