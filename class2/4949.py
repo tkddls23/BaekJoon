ca95077adeea76bcd7917916495096635fb69edb
wonseok <tkddls23@konkuk.ac.kr>
2022-10-19 02:41:04 +0900

    baekjoon_4949

---
+++ class2/4949.py
@@ -0,0 +1,31 @@
+# 4949 균형잡힌 세상
+from sys import stdin
+
+while True:
+    n = input()
+    stack = []
+
+    if n == '.':
+        break
+
+    for i in n :
+        if i == '[' or i == '(' :
+            stack.append(i)
+        elif i == ']' :
+            if len(stack) != 0 and stack[-1] == '[' :
+                stack.pop()
+            else : 
+                stack.append(']')
+                break
+        elif i == ')' :
+            if len(stack) != 0 and stack[-1] == '(' :
+                stack.pop()
+            else :
+                stack.append(')')
+                break
+
+    
+    if len(stack) == 0 :
+        print('yes')
+    else :
+        print('no')    
