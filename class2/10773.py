d41899e8bdce23f6a025fc5818ad2cc2ac49af2b
wonseok <tkddls23@konkuk.ac.kr>
2022-10-24 16:01:35 +0900

    baekjoon_10773

--- class2/10773.py
+++ class2/10773.py
@@ -6,17 +6,10 @@ l = []
 result = 0
 x = 0
 for i in range(K):
-    l.append(int(sys.stdin.readline()))
-
-while l:
-    tmp = l.pop()
+    tmp = int(sys.stdin.readline())
     if tmp == 0:
-        x +=1
-        if l[-1] != 0:
-            for i in range(x):
-                l.pop()
-            x = 0
+        l.pop()
     else:
-        result += tmp
-
-print(result)
+        l.append(tmp)
+    
+print(sum(l))
