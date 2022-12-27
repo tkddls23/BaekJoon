c1d5fb03a63a69559c6b9defccf0773a1c5487e4
Sangwoo Shin <sangwoonoel@naver.com>
2022-12-27 17:34:16 +0900

    refactor: boj_1929

--- sangwoo/1929(소수 구하기).py
+++ sangwoo/1929(소수 구하기).py
@@ -2,7 +2,7 @@ import math
 
 M, N = map(int, input().split())
 
-primeNumbers = [True for i in range(N + 1) ]
+primeNumbers = [True for i in range(N + 1)]
 
 for i in range(2, int(math.sqrt(N)) + 1):
     if primeNumbers[i] == True:
