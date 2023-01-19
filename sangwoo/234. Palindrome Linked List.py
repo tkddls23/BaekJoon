54b111869959b361915ba1763988f9a58acd07db
Sangwoo Shin <sangwoonoel@naver.com>
2023-01-19 17:44:37 +0900

    solve: 234. Palindrome Linked List

---
+++ sangwoo/234. Palindrome Linked List.py
@@ -0,0 +1,28 @@
+from collections import deque
+from typing import Optional
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        d = deque()
+
+        if not head:
+            return True
+
+        node = head
+
+        while node is not None:
+            d.append(node.val)
+            node = node.next
+
+        while len(d) > 1:
+            if d.popleft() != d.pop():
+                return False
+
+        return True
