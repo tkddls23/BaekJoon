945cc352700099a2860f33fd9ea617c4c6ab4a48
Sangwoo Shin <sangwoonoel@naver.com>
2023-01-22 23:51:16 +0900

    두번째 풀이에 대한 문제풀이 작성

--- sangwoo/LeetCode/배열/238. Product of Array Except Self.py
+++ sangwoo/LeetCode/배열/238. Product of Array Except Self.py
@@ -18,6 +18,13 @@ class Solution:
             multiplication_value = multiplication_value * nums[i]
         return result
 
+# 풀이
+# result 리스트에 왼쪽부터 오른쪽으로 누적곱을 저장한다.
+# 예를 들어 입력이 [1,2,3,4] 라면 [1,1,2,6]이 저장된다.
+# 이를 이용해서 1 * 1을 제외한 2,3,4의 곱, 2를 제외한 1과 3,4의 곱 등으로 정답을 구할 수 있다.
+# result 리스트를 반대로 for문을 돌며 1부터 시작해서 오른쪽에서 왼쪽으로 누적곱을 곱해준다.
+# 6은 (1*2*3)으로 6*1으로 4를 제외한 값을 구하고 2는 (1*2) 이므로 2*1*4 으로 3을 제외한 값을 구할 수 있다.
+
 # class Solution:
 #     def productExceptSelf(self, nums: List[int]) -> List[int]:
 #         d = deque(nums)
