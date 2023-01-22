8bc5b15fe6629212e645e0d09de40a78cbc59462
Sangwoo Shin <sangwoonoel@naver.com>
2023-01-22 23:45:13 +0900

    첫번째 풀이에 대한 문제풀이 작성

--- sangwoo/LeetCode/배열/238. Product of Array Except Self.py
+++ sangwoo/LeetCode/배열/238. Product of Array Except Self.py
@@ -1,6 +1,7 @@
 from collections import deque
 from typing import List
 
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         d = deque(nums)
@@ -23,3 +24,10 @@ class Solution:
 
         result.append(left)
         return result
+
+# 풀이
+# 자기를 제외한 곱을 구해야 하므로 while 반복문을 돌며 가장 앞 원소를 pop했다.
+# 가장 앞 원소를 pop 해야 하므로 deque 자료형을 사용했다.
+# 가장 앞 원소가 빠진 deque의 곱과 지금까지 pop된 값들의 곱을 서로 곱해주면 자신을 제외한 곱을 구할 수 있다.
+# 가장 앞 원소가 빠진 deque의 곱은 right 리스트에 저장했다.
+# 지금까지 pop된 값들의 곱은 left 변수에 저장했다.
