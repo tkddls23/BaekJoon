883f27fa0a087aea464fba11cd916828b6e2616f
Sangwoo Shin <sangwoonoel@naver.com>
2023-01-23 18:24:38 +0900

    문제 풀이 작성

--- sangwoo/LeetCode/연결 리스트/21. Merge Two Sorted Lists.py
+++ sangwoo/LeetCode/연결 리스트/21. Merge Two Sorted Lists.py
@@ -14,4 +14,10 @@ class Solution:
             list1, list2 = list2, list1
         if list1:
             list1.next = self.mergeTwoLists(list1.next, list2)
-        return list
+        return list1
+
+# 문제 풀이
+# 이미 두 연결 리스트는 정렬되어 있다.
+# 두 노드 중에 작은 값을 왼쪽에 오도록 하고
+# 다음 노드를 지금 노드의 다음 노드와 다른 노드 중에 작은 값으로 물려준다.
+# 이를 위해 자기 자신을 호출한다.
