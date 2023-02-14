ca6f49b841dac104fdd55de148edd0bc3764ee7a
Yunu <gyunu47@gmail.com>
2023-02-14 17:49:59 +0900

    feat: 프로그래머스 스타 수열 좋은 코드 추가

---
+++ yunu/프로그래머스/스타 수열/좋은 코드.py
@@ -0,0 +1,17 @@
+def solution(a):
+    dic = {}
+    check = {}
+    for i in range(len(a)):
+        dic[i] = 0
+        check[i] = -2
+
+    for i in range(len(a) - 1):
+        if a[i] != a[i+1]:
+            if check[a[i]] != i-1:
+                dic[a[i]] += 1
+                check[a[i]] = i
+            if check[a[i+1]] != i-1:
+                dic[a[i+1]] += 1
+                check[a[i+1]] = i
+
+    return max(dic.values()) * 2
