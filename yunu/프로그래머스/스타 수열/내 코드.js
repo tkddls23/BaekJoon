ea7a17e8d8b18a5eb167c07219620ad7d8016ae8
Yunu <gyunu47@gmail.com>
2023-02-14 17:40:52 +0900

    refactor: if문 줄이기

--- yunu/프로그래머스/스타 수열/내 코드.js
+++ yunu/프로그래머스/스타 수열/내 코드.js
@@ -9,19 +9,11 @@ function solution(a) {
     } else {
       const [i, length] = cache[el];
       if (length % 2) {
-        if (idx > i + 2) {
-          cache[el] = [idx, length + 3];
-        } else if (idx > i + 1) {
-          cache[el] = [idx, length + 2];
-        } else {
-          cache[el] = [idx, length];
-        }
+        const increase = idx === i + 1 ? 0 : Math.min(3, idx - i);
+        cache[el] = [idx, length + increase];
       } else {
-        if (idx > i + 1) {
-          cache[el] = [idx, length + 2];
-        } else {
-          cache[el] = [idx, length + 1];
-        }
+        const increase = Math.min(2, idx - i);
+        cache[el] = [idx, length + increase];
       }
     }
   });
@@ -39,6 +31,51 @@ function solution(a) {
 
   return maxLength;
 }
+
+/*
+// 런타임 에러 나는 코드
+function solution(a) {
+  if (a.length < 2) return 0;
+
+  const cache = {};
+
+  a.forEach((el, idx) => {
+    if (!cache[el]) {
+      if (idx > 0) cache[el] = [idx, 2];
+      else cache[el] = [idx, 1];
+    } else {
+      const [i, length] = cache[el];
+      if (length % 2) {
+        if (idx > i + 2) {
+          cache[el] = [idx, length + 3];
+        } else if (idx > i + 1) {
+          cache[el] = [idx, length + 2];
+        } else {
+          cache[el] = [idx, length];
+        }
+      } else {
+        if (idx > i + 1) {
+          cache[el] = [idx, length + 2];
+        } else {
+          cache[el] = [idx, length + 1];
+        }
+      }
+    }
+  });
+
+  Object.keys(cache).forEach(key => {
+    const [i, length] = cache[key];
+    if (length % 2) {
+      if (i === a.length - 1) cache[key] = length - 1;
+      else cache[key] = length + 1;
+    } else {
+      cache[key] = length;
+    }
+  });
+
+  return Math.max(...Object.values(cache));
+}
+*/
 
 // console.log(solution([0]));
 console.log(solution([5, 2, 3, 3, 5, 3]));
