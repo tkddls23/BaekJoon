206ce23f5d402c4eed82d9a31d29dbe1ca6a69b2
Yunu <gyunu47@gmail.com>
2023-03-06 22:02:42 +0900

    refactor: 프로그래머스 연속 펄스 부분 수열의 합

--- yunu/프로그래머스/연속 펄스 부분 수열의 합/내 코드.js
+++ yunu/프로그래머스/연속 펄스 부분 수열의 합/내 코드.js
@@ -1,26 +1,22 @@
 function solution(sequence) {
   const n = sequence.length;
 
-  let pulse = Array.from({ length: n }, (_, i) => (i % 2 === 0 ? 1 : -1));
-  let reverse = Array.from({ length: n }, (_, i) => (i % 2 === 0 ? -1 : 1));
+  let pulseSeq = sequence.map((el, i) => el * (i % 2 === 0 ? 1 : -1));
+  let reverseSeq = sequence.map((el, i) => el * (i % 2 === 0 ? -1 : 1));
 
-  let pulseMax = sequence[0] * pulse[0];
-  const pulseDp = [sequence[0] * pulse[0]];
+  let maxSum = Math.max(pulseSeq[0], reverseSeq[0]);
+  const pulseDp = [pulseSeq[0]];
+  const reverseDp = [reverseSeq[0]];
   for (let i = 1; i < n; i++) {
-    const prev = pulseDp[pulseDp.length - 1];
-    const curr = sequence[i] * pulse[i];
-    pulseDp.push(Math.max(prev + curr, curr));
-    pulseMax = Math.max(pulseMax, Math.max(prev + curr, curr));
+    const currPulse = Math.max(pulseDp[i - 1] + pulseSeq[i], pulseSeq[i]);
+    const currReverse = Math.max(
+      reverseDp[i - 1] + reverseSeq[i],
+      reverseSeq[i]
+    );
+    pulseDp.push(currPulse);
+    reverseDp.push(currReverse);
+    maxSum = Math.max(maxSum, currPulse, currReverse);
   }
 
-  let reverseMax = sequence[0] * reverse[0];
-  const reverseDp = [sequence[0] * reverse[0]];
-  for (let i = 1; i < n; i++) {
-    const prev = reverseDp[reverseDp.length - 1];
-    const curr = sequence[i] * reverse[i];
-    reverseDp.push(Math.max(prev + curr, curr));
-    reverseMax = Math.max(reverseMax, Math.max(prev + curr, curr));
-  }
-
-  return Math.max(pulseMax, reverseMax);
+  return maxSum;
 }
