330e50ee4a5930ed3783cc78b50c753619fe63dc
msdio <rlagofka1@gmail.com>
2023-03-14 12:14:30 +0900

    pg 49189

---
+++ haeram/python/pg_49189.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(n, edge):
+    #adj list
+    graph = {}
+    for i in range(1, n+1):
+        graph[i] = []
+        
+    for a, b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+        
+    #bfs
+    visited = [0 for i in range(n+1)]
+    dq = deque([1])
+    visited[1] = 1
+    
+    while dq:
+        cur = dq.popleft()
+        
+        for g in graph[cur]:
+            if(not visited[g]):
+                visited[g] = visited[cur] + 1
+                dq.append(g)
+                
+    ans = 0
+    max_val = max(visited)
+    for n in visited:
+        if(n == max_val):
+            ans += 1
+            
+    return ans
+    
+    
