878004e3902f4087b987c61709d8f71c7a4fac51
Yunu <gyunu47@gmail.com>
2023-03-15 23:09:53 +0900

    refactor: 프로그래머스 스티커 모으기(2)

--- yunu/프로그래머스/스티커 모으기(2)/내 코드.js
+++ yunu/프로그래머스/스티커 모으기(2)/내 코드.js
@@ -5,22 +5,19 @@ function solution(sticker) {
     return Math.max(...sticker);
   }
 
-  const dp0 = Array(n).fill(0);
   const dp1 = Array(n).fill(0);
+  const dp2 = Array(n).fill(0);
 
-  dp0[0] = sticker[0];
-  dp0[2] = dp0[0] + sticker[2];
-  dp1[1] = sticker[1];
-  dp1[2] = sticker[2];
-
-  let max = Math.max(dp0[0], dp0[2], dp1[1]);
-  for (let i = 3; i < n; i++) {
-    if (i < n - 1) {
-      dp0[i] = sticker[i] + Math.max(dp0[i - 2], dp0[i - 3]);
-    }
-    dp1[i] = sticker[i] + Math.max(dp1[i - 2], dp1[i - 3]);
-    max = Math.max(max, dp0[i], dp1[i]);
+  dp1[0] = sticker[0];
+  dp1[1] = dp1[0];
+  for (let i = 2; i < n - 1; i++) {
+    dp1[i] = Math.max(dp1[i - 2] + sticker[i], dp1[i - 1]);
   }
 
-  return max;
+  dp2[1] = sticker[1];
+  for (let i = 2; i < n; i++) {
+    dp2[i] = Math.max(dp2[i - 2] + sticker[i], dp2[i - 1]);
+  }
+
+  return Math.max(dp1[n - 2], dp2[n - 1]);
 }
