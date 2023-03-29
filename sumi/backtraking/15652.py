660a1292248ea552f2e07e4de7ae636e57094887
sumi-0011 <selina2000@naver.com>
2023-03-29 22:04:15 +0900

    feat: n과 m (4~5)

--- sumi/backtraking/15652.py
+++ sumi/backtraking/15652.py
@@ -1,7 +1,9 @@
 import sys
+from itertools import product
 
 sys.stdin = open('index.txt')
 input = sys.stdin.readline
+
 
 
 
@@ -10,14 +12,17 @@ if __name__ == '__main__':
     n, m =  map(int, input().split())
 
     s = []
+
     def dfs():
         # 원하는 길이의 숫자 조합을 가졌을 때, 결과값을 출력
         if len(s) == m:
             print(' '.join(map(str, s)))
             return
-        # 1부터 리스트에 추가했다가 다시 제거하기 때문에
-        # 오름차순으로 리스트에 추가될 수 밖에 없다.
+        # 1부터 리스트에 추가했다가 다시 제거하기 때문에 오름차순으로 리스트에 추가될 수 밖에 없다.
         for i in range(1,n+1):
+            # 마지막 원소보다 큰 수만 추가
+            if len(s) > 0 and s[-1] > i:
+                continue
             s.append(i)
             dfs()
             s.pop()
